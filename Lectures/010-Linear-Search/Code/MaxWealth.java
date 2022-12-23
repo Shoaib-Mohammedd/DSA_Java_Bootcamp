c2cca0f5297c83496480a39cfd28d2e2e089245f
Shoaib-Mohammedd <shuaibmuhammed149@gmail.com>
2022-12-23 23:47:43 +0300

    Linear Search

---
+++ Lectures/010-Linear-Search/Code/MaxWealth.java
@@ -0,0 +1,19 @@
+public class MaxWealth {
+    public static void main(String[] args) {
+
+    }
+    static int maximumWealth ( int [][] accounts ){
+        int ans = Integer.MIN_VALUE;
+        for (int person = 0; person < accounts.length; person++) {
+            int sum = 0;
+            for ( int account = 0; account < accounts[person].length; account++){
+                sum += accounts [ person] [account];
+            }
+            if ( sum > ans){
+                ans = sum;
+            }
+
+        }
+        return ans;
+    }
+}
