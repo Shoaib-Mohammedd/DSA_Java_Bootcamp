df4eee0357a6c751372765a443f9f7adb92cdcf7
Shoaib-Mohammedd <shuaibmuhammed149@gmail.com>
2022-12-25 22:08:26 +0300

    Easy Added

---
+++ Lectures/14-recursion/Code/easy/Nto1.java
@@ -0,0 +1,38 @@
+public class Nto1 {
+    public static void main(String[] args) {
+        funBoth(5);
+    }
+
+    static void concept(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+        concept(--n);
+    }
+
+    static void fun(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+        fun(n-1);
+    }
+
+    static void funRev(int n) {
+        if (n == 0) {
+            return;
+        }
+        funRev(n-1);
+        System.out.println(n);
+    }
+
+    static void funBoth(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+        funBoth(n-1);
+        System.out.println(n);
+    }
+}
