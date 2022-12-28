cf0a0c403df1fdf46f940e9da1b0b8adf2758cce
Shoaib-Mohammedd <shuaibmuhammed149@gmail.com>
2022-12-28 19:56:28 +0300

    Collection

---
+++ Lectures/17-OOP/Code/Collection/Maincollection.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Maincollection {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        List<Integer> list2 = new LinkedList<>();
+
+
+
+
+        List<Integer> vector = new Vector<>();
+        vector.add(45);
+        vector.add(5);
+        vector.add(15);
+        vector.add(56);
+
+        System.out.println(vector);
+    }
+}
