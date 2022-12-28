7fe15bcac1bc13aea0fde6cc15ee787f0beafc71
Shoaib-Mohammedd <shuaibmuhammed149@gmail.com>
2022-12-28 20:15:41 +0300

    Message( Greeting )

---
+++ Lectures/17-OOP/Code/Packeges/b/Greting.java
@@ -0,0 +1,5 @@
+public class Greting {
+    public static void main(String[] args) {
+        System.out.println(" I am awesome");
+    }
+}
---
+++ Lectures/17-OOP/Code/Packeges/b/Messagem.java
@@ -0,0 +1,9 @@
+public class Messagem {
+    public static void main(String[] args) {
+
+    }
+
+    public static void message() {
+        System.out.println("This course is awesome");
+    }
+}
