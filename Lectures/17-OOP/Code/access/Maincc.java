c812a2703f3630b3a597c838079ed54ba9afd6ba
Shoaib-Mohammedd <shuaibmuhammed149@gmail.com>
2022-12-28 19:51:58 +0300

    access

---
+++ Lectures/17-OOP/Code/access/Maincc.java
@@ -0,0 +1,8 @@
+public class Maincc {
+    public static void main(String[] args) {
+        A obj = new A(10, "Shoaib");
+
+        obj.getNum();
+        int n = obj.num;
+    }
+}
---
+++ Lectures/17-OOP/Code/access/ObjectDemoacc.java
@@ -0,0 +1,54 @@
+public class ObjectDemoacc {
+
+    int num;
+    float gpa;
+
+    public ObjectDemoacc(int num, float gpa) {
+        this.num = num;
+        this.gpa = gpa;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+//    @Override
+//    protected void finalize() throws Throwable {
+//        super.finalize();
+//    }
+
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return this.num == ((ObjectDemo)obj).num;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public static void main(String[] args) {
+        ObjectDemo obj1 = new ObjectDemo(12, 56.8f);
+        ObjectDemo obj2 = new ObjectDemo(12, 78.9f);
+
+        if (obj1 == obj2) {
+            System.out.println("obj1 is equal to obj2");
+        }
+
+        if (obj1.equals(obj2)) {
+            System.out.println("obj1 is equal to obj2");
+        }
+
+        System.out.println(obj1.getClass().getName());
+
+
+    }
+
+}
