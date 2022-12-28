a51966ca1e0d627b1e24e9770ce44b1b7206ec99
Shoaib-Mohammedd <shuaibmuhammed149@gmail.com>
2022-12-28 20:10:56 +0300

    nasted

---
+++ Lectures/17-OOP/Code/Inheritance/nasted/An.java
@@ -0,0 +1,12 @@
+public class An {
+    public interface NestedInterface {
+        boolean isOdd(int num);
+    }
+}
+
+//class B implements A.NestedInterface {
+//    @Override
+//    public boolean isOdd(int num) {
+//        return (num & 1) == 1;
+//    }
+//}
