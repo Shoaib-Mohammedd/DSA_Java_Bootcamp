b3caa2640872edba3a6c2c9ce02a767074aef6c9
Shoaib-Mohammedd <shuaibmuhammed149@gmail.com>
2022-12-28 19:58:48 +0300

    ExaptionHandling

---
+++ Lectures/17-OOP/Code/ExaptionHandling/Demoex.java
@@ -0,0 +1,5 @@
+public class Demoex {
+    public static void main(String[] args) {
+        Main.divide(3, 0);
+    }
+}
---
+++ Lectures/17-OOP/Code/ExaptionHandling/Mainex.java
@@ -0,0 +1,31 @@
+public class Mainex {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 0;
+        try {
+//            divide(a, b);
+            // mimicing
+            String name = "Shoaib";
+            if (name.equals("Shoaib")) {
+                throw new MyException("name is Shoaib");
+            }
+        } catch (MyException e) {
+            System.out.println(e.getMessage());
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("normal exception");
+        } finally {
+            System.out.println("this will always execute");
+        }
+
+    }
+
+    static int divide(int a, int b) throws ArithmeticException{
+        if (b == 0) {
+            throw new ArithmeticException("please do no divide by zero");
+        }
+
+        return  a / b;
+    }
+}
